e0db6a97558db932e0816395d7c8d59b76cc27e1
Fong-Suen <fong.suen@student.hu.nl>
2024-06-07 21:11:20 +0200

    Removed unnecessary stylinh

--- components/footer/company/Company.vue
+++ components/footer/company/Company.vue
@@ -1,24 +1,24 @@
 <template>
     <div>
         <h3 class="font-semibold text-blue-500">Company</h3>
-        <ul class="mt-4 space-y-2">
+        <ul class="mt-4 space-y-2 text-gray-500">
             <li>
-                <NuxtLink to="/" class="text-gray-500 hover:text-gray-800">Home</NuxtLink>
+                <NuxtLink to="/" class="hover:text-gray-800">Home</NuxtLink>
             </li>
             <li>
-                <NuxtLink to="/services" class="text-gray-500 hover:text-gray-800">Services</NuxtLink>
+                <NuxtLink to="/services" class="hover:text-gray-800">Services</NuxtLink>
             </li>
             <li>
-                <NuxtLink to="/why-us" class="text-gray-500 hover:text-gray-800">Why us?</NuxtLink>
+                <NuxtLink to="/why-us" class="hover:text-gray-800">Why us?</NuxtLink>
             </li>
             <li>
-                <NuxtLink to="/review" class="text-gray-500 hover:text-gray-800">Reviews</NuxtLink>
+                <NuxtLink to="/review" class="hover:text-gray-800">Reviews</NuxtLink>
             </li>
             <li>
-                <NuxtLink to="/who-are-we" class="text-gray-500 hover:text-gray-800">Who Are We?</NuxtLink>
+                <NuxtLink to="/who-are-we" class="hover:text-gray-800">Who Are We?</NuxtLink>
             </li>
             <li>
-                <NuxtLink to="/contact" class="text-gray-500 hover:text-gray-800">Contact</NuxtLink>
+                <NuxtLink to="/contact" class="hover:text-gray-800">Contact</NuxtLink>
             </li>
         </ul>
     </div>
--- components/footer/help/HelpCenter.vue
+++ components/footer/help/HelpCenter.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <h3 class="font-semibold text-blue-500">Help center</h3>
-        <ul class="mt-4 space-y-2">
-            <li><span class="text-gray-500">Privacy policy</span></li>
-            <li><span class="text-gray-500">Cookies</span></li>
-            <li><span class="text-gray-500">Licenses</span></li>
-            <li><span class="text-gray-500">Terms & condition</span></li>
-            <li><span class="text-gray-500">Contact</span></li>
+        <ul class="mt-4 space-y-2 text-gray-500">
+            <li>Privacy policy</li>
+            <li>Cookies</li>
+            <li>Licenses</li>
+            <li>Terms & condition</li>
+            <li>Contact</li>
         </ul>
     </div>
 </template>
